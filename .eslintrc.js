0653801e3d8b271c2249cc14749016ed53eb82fa
Nishiraj Singh <your-email@example.com>
2025-09-03 23:10:51 +0530

    Fix Vercel build configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+    'no-mixed-operators': 'warn',
+    'no-duplicate-case': 'warn'
+  }
+};
